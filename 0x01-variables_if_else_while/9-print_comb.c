1e03b1517a600899e08dd736c8b939ab50addfca
Raymond <owizbluez@gmail.com>
2022-09-12 12:59:16 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ *
+ * Return: 0 = if successful
+ *
+ */
+
+
+int main(void)
+{
+
+	int item;
+
+		for (item = '0'; item <= '9'; item++)
+
+		{
+
+			putchar(item);
+
+
+			if (item != '9')
+
+			{
+
+				putchar(',');
+
+				putchar(' ');
+
+			}
+
+		} putchar('\n');
+
+	return (0);
+
+}
