98ef40ded889ba9c4c89dccff0a24865c37459b5
Raymond <owizbluez@gmail.com>
2022-09-12 10:51:44 -0700

    update for task 2 and 3

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,20 +6,18 @@
  *
  */
 int main(void)
+
 {
-		int d1, d2, n = 0;
-	for (d1 = '0'; d1 <= '9'; d1++)
+
+	char start;
+
+	for (start = 'a'; start  <= 'z'; start++)
 	{
-		for (d2 = d1 + 1; d2 <= '9'; d2++, n++)
-		{
-			if (n > 0)
-				{
-			putchar(',');
-			putchar(' ');
-				}
-			putchar(d1);
-			putchar(d2);
-		}
-	}	putchar('\n');
-		return (0);
+
+		putchar(start);
+
+	}
+	putchar('\n');
+
+	return (0);
 }
