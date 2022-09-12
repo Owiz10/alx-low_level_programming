7573107f3f4c1b625774ba3432e744bd02db2548
Raymond <owizbluez@gmail.com>
2022-09-12 12:00:52 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
-
 /**
  * main - entry
  *
  *  Return: 0 (when successful)
  *
  */
-
 int main(void)
-
 {
 		char set1;
 		char set2;
 
 		for (set1 = 'a'; set1 <= 'z'; set1++)
-		putchar(set1);
+			putchar(set1);
 
 		for (set2 = 'A'; set2 <= 'Z'; set2++)
 			putchar(set2);
