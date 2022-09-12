58152e063bbb8944dae41998ebea3ff1b3a0134c
Raymond <owizbluez@gmail.com>
2022-09-12 12:21:34 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+
+		char number = '0';
+
+		for (number = '0'; number <= '9'; number++)
+
+		putchar(number);
+
+		putchar('\n');
+
+		return (0);
+
+}
