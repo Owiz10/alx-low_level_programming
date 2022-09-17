9c322f8455b0f29796c52a5b950733e29f297e9f
Raymond <owizbluez@gmail.com>
2022-09-16 17:19:59 -0700

    task 6-print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * print_line	-	prints a line
+ *
+ * @n:	Defines the length of line to be printed
+ *
+ * Return:	Always 0
+ *
+ */
+
+void print_line(int n)
+{
+
+		if (n <= 0)
+		{
+
+			_putchar('\n');
+
+			return (0);
+
+		}
+
+		else
+
+		{
+
+				while (n > 0)
+				{
+
+					_putchar('_');
+
+					n--;
+
+				}
+
+		_putchar('\n');
+
+		return (0);
+
+		}
+
+}
