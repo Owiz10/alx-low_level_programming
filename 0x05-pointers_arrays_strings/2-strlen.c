6fc9dc361bb22377fcf4ce505d10aa5d9a6e192c
Raymond <owizbluez@gmail.com>
2022-09-19 16:58:49 -0700

    task 2- string length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _strlen - a function that returns the length of a string
@@ -26,5 +26,5 @@ int _strlen(char *s)
 
 		}
 
-					return (sum);
+	return (sum);
 }
