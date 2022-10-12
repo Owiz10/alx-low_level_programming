fbcf30de6eae91c453d526ba2cdf5c0f692d3da5
Raymond <owizbluez@gmail.com>
2022-10-12 06:03:33 -0700

    a function to print small letters of the english alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lower case
+ *
+ * Return: Always 0.
+ *
+ */
+
+void print_alphabet(void)
+
+{
+
+		char small;
+
+		for (small = 'a'; small <= 'z'; small++)
+
+			_putchar(small);
+
+			_putchar('\n');
+
+}
