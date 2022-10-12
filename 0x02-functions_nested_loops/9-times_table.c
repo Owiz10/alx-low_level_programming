226e296e20c8c83751c8faa134cbea4d7fcdc597
Raymond <owizbluez@gmail.com>
2022-10-12 15:39:02 -0700

    task 9- a function to print out the 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,24 +16,12 @@ void times_table(void)
 	for (start = 0; start <= 9; start++)
 
 	{
-
 		for (stop = 0; stop <= 9; stop++)
 
 		{
-
 			int result = stop * start;
 
-			if (start == 0)
-
-				printf("%i", result);
-
-			if (result > 10)
-
-				printf(",  %i", result);
-
-			else
-
-				printf(", %i", result);
+			printf(", %i", result);
 
 		}
 
