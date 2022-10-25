dff1c9b7d149d2940cf5ea6b959763480cee1161
Raymond <owizbluez@gmail.com>
2022-10-25 08:18:56 -0700

    taask 1- a function that returns the number of elements in a linked list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -20,4 +20,4 @@ size_t print_listint_safe(const listint_t *head);
 listint_t *find_listint_loop(listint_t *head);
 size_t free_listint_safe(listint_t **h);
 
-#endif "LISTS_H"
+#endif /* "LISTS_H" */
