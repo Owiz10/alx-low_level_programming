f855ea8cef8f9c77bdf1427928070fdd52c66b78
Raymond <owizbluez@gmail.com>
2022-11-03 10:35:46 -0700

    task 1 - a function that concatenates 2 strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,15 +22,21 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int s1_len, s2_len, i;
 
 	if (s1 == NULL)
+
 		s1 = "";
+
 	if (s2 == NULL)
+
 		s2 = "";
 
 	for (s1_len = 0; s1[s1_len] != '\0'; s1_len++)
 
 		;
+
 	for (s2_len = 0; s2[s2_len] != '\0'; s2_len++)
+
 		;
+
 	str = malloc(s1_len + s2_len + 1);
 
 	if (n >= s2_len)
@@ -48,7 +54,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	str[s1_len + n] = '\0';
 
 	if (str == NULL)
-
 		return (NULL);
 
 	return (str);
