50dbc4ca0eb7d9796a5d6302d9b5e84b8c38b23e
Raymond <owizbluez@gmail.com>
2022-11-17 02:27:47 -0800

    correction

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,34 +1,37 @@
 #include <stdio.h>
 
 /**
- * main - entry
+ * main - sums nultplies of 3 or 5
  *
- * Return: 0
+ * Description: multiples between 0 and 1024
  *
+ * Return: Always(0) Success
  */
 
 int main(void)
-
 {
+		int start_num, end_num, total;
 
-	int start = 1;
+		end_num = 1024;
 
-	int max = 1024;
+		total = 0;
 
-	int sum = 0;
-
-	for (start = 1; start < max; start++)
-
+		for (start_num = 0; start_num < end_num; start_num++)
+		{
+			if ((start_num % 3 == 0) || (start_num % 5 == 0))
 			{
-
-				if ((start % 3 == 0) || (start % 5 == 0))
-
-					sum += start;
-
+				total = total + start_num;
+			}
+			else
+			{
+			continue;
 			}
 
-	printf("%d\n", sum);
+		}
 
-	return (0);
+		printf("%d", total);
 
+		printf("\n");
+
+		return (0);
 }
