680368d3238b897863bdf8bbd0bd1fc56bb6a0dc
Raymond <owizbluez@gmail.com>
2022-11-17 09:07:21 -0800

    correction

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,38 +12,40 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+char *str;
 
-	char *str;
+unsigned int s1_len, s2_len, i;
 
-	unsigned int s1_len, s2_len, i;
 
-	if (s1 == NULL)
-	s1 = "";
+if (s1 == NULL)
+s1 = "";
 
-	if (s2 == NULL)
-	s2 = "";
+if (s2 == NULL)
+s2 = "";
 
-	for (s1_len = 0; s1[s1_len] != '\0'; s1_len++)
-	;
+for (s1_len = 0; s1[s1_len] != '\0'; s1_len++)
+;
 
-	for (s2_len = 0; s2[s2_len] != '\0'; s2_len++)
-	;
-	
-	str = malloc(s1_len + s2_len + 1);
+for (s2_len = 0; s2[s2_len] != '\0'; s2_len++)
+;
 
-	if (n >= s2_len)
-		n = s2_len;
 
-	for (i = 0; s1[i] != '\0'; i++)
-		str[i] = s1[i];
+str = malloc(s1_len + s2_len + 1);
 
-	for (i = 0; i < n; i++)
-		str[s1_len + i] = s2[i];
 
-	str[s1_len + n] = '\0';
+if (n >= s2_len)
+n = s2_len;
 
-	if (str == NULL)
-	return (NULL);
+for (i = 0; s1[i] != '\0'; i++)
+str[i] = s1[i];
 
-	return (str);
+for (i = 0; i < n; i++)
+str[s1_len + i] = s2[i];
+
+str[s1_len + n] = '\0';
+
+if (str == NULL)
+return (NULL);
+
+return (str);
 }
