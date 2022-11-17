8519f1dad2aebcc940d25d311d125c4922544890
Raymond <owizbluez@gmail.com>
2022-11-17 08:35:43 -0800

    correction

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,60 +1,48 @@
+#include "main.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * string_nconcat -	Adds specific number of bytes of one string to another
- *
- * @s1:		The first string
- *
- * @s2:	The string to be added to @s1
- *
- * @n:	Number of bytes of @s2 to be added to @s1
- *
- * Return:	Pointer to memory containing s1 + s2
- *
- * NULL if the function fails
- *
+ * string_nconcat - concatenates two strings
+ * @s1: first string to concatenate
+ * @s2: second string to concatenate
+ * @n: number of bytes
+ * Return: pointer to new allocated space
+ * If it fails, return NULL
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+		char *concat_str;
+			unsigned int itr_s1, itr_s2, itr_out, i;
 
-	char *new;
+				if (s1 == NULL)
+					s1 = "";
 
-	unsigned int len1, len2;
+				if (s2 == NULL)
+					s2 = "";
 
-	if (s1 == NULL)
+					for (itr_s1 = 0; s1[itr_s1] != '\0'; itr_s1++)
+						;
 
-		s1 = "";
+					for (itr_s2 = 0; s2[itr_s2] != '\0'; itr_s2++)
+						;
 
-	if (s2 == NULL)
+				if (n > itr_s2)
+					n = itr_s2;
 
-		s2 = "";
+				itr_out = itr_s1 + n;
 
-	len1 = strlen(s1);
+				concat_str = malloc(itr_out + 1);
 
-	len2 = strlen(s2);
+				if (concat_str == NULL)
+					return (NULL);
+				for (i = 0; i < itr_out; i++)
 
-	if (n > len2)
+				if (i < itr_s1)
 
-		n = len2;
-
-	new = malloc(sizeof(char) * (len1 + len2) + 1);
-
-	if (!new)
-
-		return (NULL);
-
-	else
-
-			{
-
-				strcpy(new, s1);
-
-				strncat(new, s2, n);
-
-				return (new);
-
-			}
-
+					concat_str[i] = s1[i];
+				else
+				concat_str[i] = s2[i - itr_s1];
+				concat_str[i] = '\0';
+				return (concat_str);
 }
